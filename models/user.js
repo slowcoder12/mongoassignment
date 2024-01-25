f4e9b81b0fcae799cdbdff03c139d988e3ceacc1
slowcoder12 <132445668+slowcoder12@users.noreply.github.com>
2024-01-25 08:20:50 +0530

    creating users table

--- models/user.js
+++ models/user.js
@@ -1,16 +1,21 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const { getDb } = require("../util/database");
 
-const sequelize = require('../util/database');
+const db = require("../util/database").getDb;
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
-
-module.exports = User;
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
+  save() {
+    const db = getDb();
+    return db.collection("users").insertOne(this);
+  }
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new mongodb.ObjectId(userId) });
+  }
+}
